8557fcb4837bac7547581cad7a6cea9a6b534649
太武 <122966582+linkdesigners@users.noreply.github.com>
2023-04-14 17:42:18 +0800

    add fav button

--- myscripts.js
+++ myscripts.js
@@ -19,3 +19,29 @@ function copyURL(){
         setTimeout(function(){ x.className = x.className.replace("show", ""); }, 2600);
 
 }
+
+
+function addFavorite() {
+        var url = window.location;
+        var title = document.title;
+        var ua = navigator.userAgent.toLowerCase();
+        if (ua.indexOf("360se") > -1) {
+            alert("尴尬，由于 360 浏览器功能限制，请按 Ctrl+D 手动收藏。");
+        }
+        else if (ua.indexOf("msie 8") > -1) {
+            window.external.AddToFavoritesBar(url, title); //IE8
+        }
+        else if (document.all) {//IE类浏览器
+          try{
+           window.external.addFavorite(url, title);
+          }catch(e){
+           alert('尴尬，当前浏览器不支持一键收藏,请按 Ctrl+D 手动收藏。');
+          }
+        }
+        else if (window.sidebar) {//firfox等浏览器；
+            window.sidebar.addPanel(title, url, "");
+        }
+        else {
+            alert('尴尬，当前浏览器不支持一键收藏,请按 Ctrl+D 手动收藏。');
+        }
+    }    
